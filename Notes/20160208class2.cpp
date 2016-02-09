b31992d8048b78041dffada74541c724798e47e9
byrnedawg <greg.byrne53@gmail.com>
2016-02-09 03:26:26 +0000

    updated class 2 notes

--- Notes/20160208class2.cpp
+++ Notes/20160208class2.cpp
@@ -18,6 +18,7 @@ float y2 = int(3.6) * 3; // another way to do the same looks like a function
 float y3 = int 
 
 // Arrays constant size arrays
+int y; //global variable are always 0
 
 int a[100]; // 100 int = 100* sizeof(int) = 400 bytes
 int b[500000000]; // too big for windows
@@ -26,6 +27,37 @@ int b[n]; //dynamically allocated on the heap
 int c[2*5*3];
 int d[n]; // int* p = new int[n];
 // there is a safe array type Array<int,100> a;
+int gr[5]; // values are random garbage
+int gr[5] = { 95, 80, 90, 65}; // gr[4] == 0;
+
+// pass by reference
+double mean(const int g[], int length)
+{
+    
+}
+
+void stats (const int g[], int length, double& mean, int& max, int& min)
+}
+
+// Strings
+char a[] = "testing"; //a[7] = '\0' NUL character
+a[7] = 'x'; // memory bug!
+
+//n gon problem
+n = 5
+if odd mult by 3 add 1
+if even divede by 2
+// look at his one pager on cmath
 
 
+// notes on reference
+int& r = a;
+r = 2;
+a is now = 2;
+// this is illegal
+int& q = b;
+q = 7;
+
+const int& s = a; // animals analogy
+a = 19;
 
